3ade4d9cab6d1171f44fbee21e2fc2914f1f02f6
woojin <woojin317@gmail.com>
2025-04-05 01:00:22 +0900

    5

--- schedule/lib/features/schedule/presentation/pages/add_schedule_page.dart
+++ schedule/lib/features/schedule/presentation/pages/add_schedule_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import '../widgets/title_input.dart';
+import '../widgets/date_time_selector.dart';
 
 class AddSchedulePage extends StatefulWidget {
   const AddSchedulePage({super.key});
@@ -10,6 +11,8 @@ class AddSchedulePage extends StatefulWidget {
 
 class _AddSchedulePageState extends State<AddSchedulePage> {
   final _titleController = TextEditingController();
+  final _startDate = DateTime(2023, 12, 12, 22); // 12월 12일 오후 10시
+  final _endDate = DateTime(2023, 12, 15, 23);   // 12월 15일 오후 11시
 
   @override
   void dispose() {
@@ -32,6 +35,17 @@ class _AddSchedulePageState extends State<AddSchedulePage> {
                 controller: _titleController,
                 onChanged: (value) {
                   // TODO: 제목 변경 처리
+                },
+              ),
+              const SizedBox(height: 32),
+              DateTimeSelector(
+                startDate: _startDate,
+                endDate: _endDate,
+                onStartDateTap: () {
+                  // TODO: 시작 날짜/시간 선택
+                },
+                onEndDateTap: () {
+                  // TODO: 종료 날짜/시간 선택
                 },
               ),
             ],
---
+++ schedule/lib/features/schedule/presentation/widgets/date_time_selector.dart
@@ -0,0 +1,122 @@
+import 'package:flutter/material.dart';
+
+class DateTimeSelector extends StatelessWidget {
+  final DateTime startDate;
+  final DateTime endDate;
+  final VoidCallback? onStartDateTap;
+  final VoidCallback? onEndDateTap;
+
+  const DateTimeSelector({
+    super.key,
+    required this.startDate,
+    required this.endDate,
+    this.onStartDateTap,
+    this.onEndDateTap,
+  });
+
+  String _getWeekday(DateTime date) {
+    const weekdays = ['월', '화', '수', '목', '금', '토', '일'];
+    return weekdays[date.weekday - 1];
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        const Text(
+          '날짜 및 시간',
+          style: TextStyle(
+            fontSize: 14,
+            fontWeight: FontWeight.w400,
+            height: 1.4,
+            letterSpacing: -0.025,
+            color: Colors.black,
+            fontFamily: 'Pretendard',
+          ),
+        ),
+        const SizedBox(height: 5),
+        Row(
+          children: [
+            GestureDetector(
+              onTap: onStartDateTap,
+              child: Container(
+                width: 150,
+                height: 104,
+                padding: const EdgeInsets.symmetric(horizontal: 26, vertical: 30),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      '${startDate.month}월 ${startDate.day}일 (${_getWeekday(startDate)})',
+                      style: const TextStyle(
+                        fontSize: 16,
+                        fontWeight: FontWeight.w600,
+                        height: 1.4,
+                        letterSpacing: -0.025,
+                        color: Colors.black,
+                        fontFamily: 'Pretendard',
+                      ),
+                    ),
+                    const SizedBox(height: 4),
+                    Text(
+                      '오후 ${startDate.hour > 12 ? startDate.hour - 12 : startDate.hour}시',
+                      style: const TextStyle(
+                        fontSize: 14,
+                        fontWeight: FontWeight.w400,
+                        height: 1.4,
+                        letterSpacing: -0.025,
+                        color: Colors.black,
+                        fontFamily: 'Pretendard',
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            const Padding(
+              padding: EdgeInsets.symmetric(horizontal: 8),
+              child: Icon(Icons.arrow_forward, size: 20),
+            ),
+            GestureDetector(
+              onTap: onEndDateTap,
+              child: Container(
+                width: 150,
+                height: 104,
+                padding: const EdgeInsets.symmetric(horizontal: 26, vertical: 30),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      '${endDate.month}월 ${endDate.day}일 (${_getWeekday(endDate)})',
+                      style: const TextStyle(
+                        fontSize: 16,
+                        fontWeight: FontWeight.w600,
+                        height: 1.4,
+                        letterSpacing: -0.025,
+                        color: Colors.black,
+                        fontFamily: 'Pretendard',
+                      ),
+                    ),
+                    const SizedBox(height: 4),
+                    Text(
+                      '오후 ${endDate.hour > 12 ? endDate.hour - 12 : endDate.hour}시',
+                      style: const TextStyle(
+                        fontSize: 14,
+                        fontWeight: FontWeight.w400,
+                        height: 1.4,
+                        letterSpacing: -0.025,
+                        color: Colors.black,
+                        fontFamily: 'Pretendard',
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+      ],
+    );
+  }
+} 
